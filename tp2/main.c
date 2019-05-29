3fa5cbc33a7a106d930986f03835262b6d1f0492
Andy <andydile@gmail.com>
2019-05-29 14:49:56 -0300

    Haciendo

--- tp2/main.c
+++ tp2/main.c
@@ -5,6 +5,7 @@
 #include "ArrayEmployes.h"
 
 #define ELEMENTS 1000 //Nómina
+#define ELEMENTSSEC 5
 
 typedef struct
 {
@@ -16,13 +17,33 @@ typedef struct
     int isEmpty;
 }Employee;
 
-void initEmployees(Employee* list, int len);
+typedef struct
+{
+    int id;
+    char description[20];
+}Sectors;
+
+int initEmployees(Employee* list, int len);
+void showSectors(Sectors* listsec, int lensec);
+int addEmployee(Employee* list, int len, int id, char name[],char
+lastName[],float salary,int sector, Sectors* listsec, int lensec);
 
 int main()
 {
+    int r; //esta variable sirve para el retorno
     Employee arrayEmployees[ELEMENTS];
-    initEmployees(arrayEmployees, ELEMENTS);
-    return 0;
+    Sectors arraySectors[ELEMENTSSEC] = {
+                                {1, "Perfumery"},
+                                {2, "Human rights"},
+                                {3, "Economy"},
+                                {4, "Systems"},
+                                {5, "Transport"}
+                                };
+    r = initEmployees(arrayEmployees, ELEMENTS);
+    //showSectors(arraySectors, ELEMENTSSEC);
+    addEmployee(arrayEmployees, ELEMENTS, id, nam)
+
+    return r;
 }
 
 /** \brief To indicate that all position in the array are empty,
@@ -33,13 +54,14 @@ int main()
  * \return int Return (-1) if Error [Invalid length or NULL pointer] - (0) if Ok
  *
  */
-void initEmployees(Employee* list, int len)
+int initEmployees(Employee* list, int len)
 {
     int i;
     for(i=0; i<len; i++)
     {
          list[i].isEmpty = 0;
     }
+    return 0;
 }
 
 /** \brief add in a existing list of employees the values received as parameters
@@ -55,9 +77,52 @@ void initEmployees(Employee* list, int len)
  *  free space] - (0) if Ok
  */
 int addEmployee(Employee* list, int len, int id, char name[],char
-lastName[],float salary,int sector)
+lastName[],float salary,int sector, Sectors* listsec, int lensec)
 {
- return -1;
+    int indice;
+    int i;
+
+    for(i=0; i<len; i++)
+    {
+        if(list[i].isEmpty == 0)
+        {
+            indice = i;
+            break;
+        }
+    }
+
+    printf("Enter name: ");
+    gets(list[indice].name);
+
+    printf("Enter last name: ");
+    gets(list[indice].lastName);
+
+    printf("Enter salary: ");
+    scanf("%f", &list[indice].salary);
+
+    showSectors(listsec, lensec);
+
+    printf("Enter sector ID: ");
+    scanf("%d", &list[indice].sector);
+
+
+    list[indice].isEmpty = 1;
+
+    return 0;
 }
 
+void showSectors(Sectors* listsec, int lensec)
+{
+    int i;
 
+    system("cls");
+
+    printf("*** SECTORES ***\n");
+    printf("________________\n");
+    printf("ID        SECTOR\n");
+
+    for(i=0; i<lensec; i++)
+    {
+        printf("%d        %10s\n", listsec[i].id, listsec[i].description);
+    }
+}
